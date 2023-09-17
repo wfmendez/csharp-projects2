4d7203dc0eb447733b9746dae527329e79b8e1e5
wfmendez <wuillian.f.mendez@gmail.com>
2023-09-16 22:54:39 -0400

    Finishing Learning Activity - C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guessNumber = -1;
+            int guesses = 0;
+
+            while (guessNumber != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guessNumber = int.Parse(Console.ReadLine());
+                guesses++;
+
+                if (magicNumber > guessNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guessNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! Your guesses are {guesses}");
+                }
+
+            }
+            Console.WriteLine("Do you want to play again? (Yes/No) ");
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer != "yes")
+            {
+                playAgain = false;
+            }
+        }
+
     }
 }
