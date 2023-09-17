5a384aa14a4fa470d86e7300f020ca158786212e
wfmendez <wuillian.f.mendez@gmail.com>
2023-09-16 20:59:51 -0400

    Add Camel Case to Variables to C# Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,13 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        string first = Console.ReadLine();
+        string firstName = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string last = Console.ReadLine();
+        string lastName = Console.ReadLine();
 
         Console.WriteLine();
 
-        Console.WriteLine($"Your name is {last}, {first} {last}.");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
