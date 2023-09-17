71d91878b5a1d77d0d9a3a817376b41b64fabd80
wfmendez <wuillian.f.mendez@gmail.com>
2023-09-16 23:35:10 -0400

    Finishing Learning Activity - C# Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        bool addNumber = true;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (addNumber)
+        {
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 0)
+            {
+                addNumber = false;
+            }
+            else 
+            {
+               numbers.Add(number); 
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
