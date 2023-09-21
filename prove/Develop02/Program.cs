95cbe3e742ddf387afe0da60d8d758fd8bf88271
wfmendez <wuillian.f.mendez@gmail.com>
2023-09-21 12:33:47 -0400

    Starting the code for Journal Program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        Journal theJournal = new Journal();
+        Entry anEntry = new Entry();
+        anEntry.Display();
+
         Console.WriteLine("Hello Develop02 World!");
     }
 }
