541428ffab10a26c4b6ca4287bf0598ad999d51e
wfmendez <wuillian.f.mendez@gmail.com>
2023-09-21 12:29:25 -0400

    Adding stub of Journal Class

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,26 @@
+using System.Runtime.CompilerServices;
+
+public class Journal
+{
+    public List<Entry> _entries;
+
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+
+    public void DisplayAll()
+    {
+
+    }
+
+    public void SaveToFile(string file)
+    {
+
+    }
+
+    public void LoadToFile(string file)
+    {
+        
+    }
+}
