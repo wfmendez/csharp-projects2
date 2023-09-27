2f35c3b58aadfd4141b1de66f1d1d0f5e8137828
wfmendez <wuillian.f.mendez@gmail.com>
2023-09-27 12:50:04 -0400

    Finishing requirements of Journal Program

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,10 +1,23 @@
-public class Entry{
-    public string _date;
+public class Entry
+{
+    // Entry properties, such as date, question, and  text.
+    public string Date { get; set; }
     public string _promptText;
-    public string _entry;
+    public string _entryText;
+
+    // Creation of a new entry with date, question and answer.
+    public Entry(string date, string promptText, string entryText)
+    {
+        Date = date;
+        _promptText = promptText;
+        _entryText = entryText;
+    }
 
     public void Display()
     {
-        
+        Console.WriteLine($"Date: {Date}");
+        Console.WriteLine($"Prompt: {_promptText}");
+        Console.WriteLine($"Entry: {_entryText}");
+        Console.WriteLine();
     }
 }
